c8745fffcb7b0945e07d329ec57d62647356997d
gleb-skobinsky <glebgytnik@gmail.com>
2023-04-30 08:29:38 -0700

    Prepare dependencies and team id

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -35,6 +35,8 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+                implementation(compose.materialIconsExtended)
+                implementation(compose.material3)
             }
         }
         val androidMain by getting {
