8dcd0234b3fb2bdef8e308159e6c58f1070f50a6
gleb-skobinsky <glebgytnik@gmail.com>
2023-05-09 20:32:38 +0300

    Proper unique ID in server

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,11 @@
+const uuid = require("uuid");
+
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ host: "127.0.0.1", port: 8082 });
 
-wss.getUniqueID = function () {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + "-" + s4();
-};
-
 wss.on("connection", (ws) => {
-  ws.id = wss.getUniqueID();
+  ws.id = uuid.v4();
   console.log(`New client connected with id: ${ws.id}`);
 
   ws.onmessage = ({ data }) => {
