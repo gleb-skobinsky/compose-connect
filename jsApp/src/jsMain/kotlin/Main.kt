a2373a7d1ad1744834fbfdae9eb0a369d1d2b371
gleb-skobinsky <glebgytnik@gmail.com>
2023-05-11 09:25:55 +0300

    Add special browser viewport window

--- jsApp/src/jsMain/kotlin/Main.kt
+++ jsApp/src/jsMain/kotlin/Main.kt
@@ -1,21 +1,11 @@
-import androidx.compose.ui.window.Window
-import kotlinx.browser.document
-import kotlinx.browser.window
 import org.jetbrains.skiko.wasm.onWasmReady
-import org.w3c.dom.HTMLCanvasElement
 
 
 fun main() {
-    resizeCanvas()
     onWasmReady {
-        Window {
+        BrowserViewportWindow("ComposeConnect") {
             Application()
         }
     }
 }
 
-private fun resizeCanvas() {
-    val wasmCanvas = document.getElementById("ComposeTarget") as HTMLCanvasElement
-    wasmCanvas.width = document.body?.clientWidth ?: window.innerWidth
-    wasmCanvas.height = document.body?.scrollHeight ?: window.innerHeight
-}
