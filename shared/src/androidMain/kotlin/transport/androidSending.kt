809486d13c2e7c8710c8a055e715ad5688b14908
gleb-skobinsky <glebgytnik@gmail.com>
2023-09-03 13:08:37 +0300

    Websocket handling in better OOP

--- shared/src/androidMain/kotlin/transport/androidSending.kt
+++ shared/src/androidMain/kotlin/transport/androidSending.kt
@@ -1,13 +1,5 @@
 package transport
 
-import data.Message
-import io.ktor.client.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.client.request.*
-import io.ktor.http.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ensureActive
-import kotlinx.coroutines.withContext
 import java.util.*
 
 actual fun getTimeNow(): String = Calendar.getInstance().time.toString()
--- shared/src/iosMain/kotlin/transport/sending.kt
+++ shared/src/iosMain/kotlin/transport/sending.kt
@@ -1,14 +1,5 @@
 package transport
 
-import data.Message
-import io.ktor.client.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.client.request.*
-import io.ktor.http.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ensureActive
-import kotlinx.coroutines.withContext
-
 actual fun getTimeNow(): String = ""
 
 actual val localHost: String = "localhost"
