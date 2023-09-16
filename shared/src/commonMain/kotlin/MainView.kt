eabd6a3fec5c7c27d9e62a0174fe8a4cab1e1787
gleb-skobinsky <glebgytnik@gmail.com>
2023-09-16 21:39:13 +0300

    Successful login

--- shared/src/commonMain/kotlin/MainView.kt
+++ shared/src/commonMain/kotlin/MainView.kt
@@ -1,12 +1,10 @@
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import composables.AuthScreen
 import composables.Conversation
 import data.MainViewModel
